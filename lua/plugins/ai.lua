ed18d1493a1e8a4b338f1fcf0e783bc729007ec0
Oleksandr Ladnov <coldover@gmail.com>
2025-02-11 10:18:15 +0200

    remove llm

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,10 +1,1 @@
-return {
-  {
-    "huggingface/llm.nvim",
-    opts = {
-      backend = "ollama",
-      model = "deepseek-coder:base",
-      url = "http://localhost:11434",
-    },
-  },
-}
+return {}
